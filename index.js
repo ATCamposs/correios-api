dc7aa6d1fe7c0dece71826b6ffff3a7791f670f8
ATCamposs <andre.tatibano@gmail.com>
2021-10-03 10:39:16 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import nanoexpress from 'nanoexpress';
+import axios from 'axios';
+import {JSDOM} from 'jsdom';
+
+const app = nanoexpress();
+
+app.get('/code/:code', async (req, res) => {
+    let trackingCode = req.params.code;
+    try {
+        const { data } = await axios.get(`https://www.linkcorreios.com.br/${trackingCode}`);
+        const dom = new JSDOM(data, {
+          runScripts: "dangerously",
+          resources: "usable"
+        });
+        const { document } = dom.window;
+        const firstResponseCardTracking= {
+            status: document.querySelector('.linha_status li:nth-child(1) > b').innerHTML,
+            date: document.querySelector('.linha_status li:nth-child(2)').innerHTML.replace('Data  : ', '').replace(' | Hora:', ''),
+            local: document.querySelector('.linha_status li:nth-child(3)').innerHTML.replace('Local: ', '')
+        };
+        return res.send(JSON.stringify(firstResponseCardTracking));
+      } catch (error) {
+        throw error;
+      }
+
+});
+
+app.listen(3000, '0.0.0.0');
